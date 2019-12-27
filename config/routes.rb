e89efb6aa315b8c865cb1eabe0c1073cb336c8a4
KKennedyCodes <holmstedtk@gmail.com>
2019-12-27 05:09:36 -0800

    Setting up basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :students, only: [:index, :show, :create]
+  resources :teachers, only: [:show, :create]
+  resources :courses
+  resources :sessions
+  resources :questions
+  resources :answers
+  resources :statuses
+  
+  # post "/rentals/:title/check-out", to: "rentals#check_out", as: "check_out"
+  # get "/rentals/overdue", to: "rentals#overdue", as: "overdue"
+  
+  root 'teachers#show'
 end
